b4acf3d11bb706ef08d2db42f33ef5f0b0f1c528
shahab2022skipq <shahab.sarwar.skipq@gmail.com>
2023-04-27 02:08:53 +0500

    First

---
+++ components/Widget.tsx
@@ -0,0 +1,32 @@
+import { LiFiWidget } from '@lifi/widget';
+import { WidgetEvents } from './WidgetEvents';
+
+export const Widget = () => {
+  return (
+    <>
+      <WidgetEvents />
+      <LiFiWidget
+        config={{
+ // deny chains
+ chains: {
+  deny: [100],
+},
+chains: {
+  deny: [250],
+},
+chains: {
+  deny: [10,43114,250,25,66,100,122,1284,1285,42220,1313161554,106,288,9001,1666600000,128,137,1],
+},
+          
+          containerStyle: {
+
+            
+            border: `1px solid rgb(234, 234, 234)`,
+            borderRadius: '16px',
+          },
+        }}
+        integrator="nextjs-example"
+      />
+    </>
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import { LoadingIndicator } from '../components/LoadingIndicator';
+
+export const LiFiWidgetNext = dynamic(
+  () => import('../components/Widget').then((module) => module.Widget) as any,
+  {
+    ssr: false,
+    loading: () => <LoadingIndicator />,
+  },
+);
+
+const Home: NextPage = () => {
+  return <LiFiWidgetNext />;
+};
+
+export default Home;
