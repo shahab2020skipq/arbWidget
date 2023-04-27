fd85e65925535d4bff7d3fe12e1ac1264cebbdea
shahab2022skipq <104347701+shahab2020skipq@users.noreply.github.com>
2023-04-27 22:45:33 +0500

    added theme

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -7,18 +7,34 @@ export const Widget = () => {
       <WidgetEvents />
       <LiFiWidget
         config={{
- // deny chains
 
 chains: {
   deny: [56,100,250,10,43114,250,25,66,100,122,1284,1285,42220,1313161554,106,288,9001,1666600000,128,137,1],
 },
           
-          containerStyle: {
-
-            
-            border: `1px solid rgb(234, 234, 234)`,
-            borderRadius: '16px',
-          },
+         containerStyle: {
+        border: `1px solid ${
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'rgb(66, 66, 66)'
+            : 'rgb(234, 234, 234)'
+        }`,
+        borderRadius: '16px',
+      },
+      theme: {
+        palette: {
+          primary: { main: '#9900d1' },
+          secondary: { main: '#F5B5FF' },
+        },
+        shape: {
+          borderRadius: 0,
+          borderRadiusSecondary: 0,
+        },
+        typography: {
+          fontFamily: 'Comic Sans MS',
+        },
+      },
+      appearance: 'dark',
+      disableAppearance: true,
         }}
         integrator="nextjs-example"
       />
