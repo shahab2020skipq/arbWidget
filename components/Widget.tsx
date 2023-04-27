03b413610dad139a05fc20998fdcea3ce9264a1d
shahab2022skipq <104347701+shahab2020skipq@users.noreply.github.com>
2023-04-27 22:57:54 +0500

    Fixed theme issues

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,41 +1,42 @@
-import { LiFiWidget } from '@lifi/widget';
+import { LiFiWidget,WidgetConfig  } from '@lifi/widget';
 import { WidgetEvents } from './WidgetEvents';
+import { useMemo } from 'react'
 
 export const Widget = () => {
+  const widgetConfig: WidgetConfig = useMemo(() => ({
+    chains: {
+      deny: [56,100,250,10,43114,250,25,66,100,122,1284,1285,42220,1313161554,106,288,9001,1666600000,128,137,1],
+    },
+    containerStyle: {
+      border: `1px solid ${
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'rgb(66, 66, 66)'
+          : 'rgb(234, 234, 234)'
+      }`,
+      borderRadius: '16px',
+    },
+    theme: {
+      palette: {
+        primary: { main: '#9900d1' },
+        secondary: { main: '#F5B5FF' },
+      },
+      shape: {
+        borderRadius: 0,
+        borderRadiusSecondary: 0,
+      },
+      typography: {
+        fontFamily: 'Comic Sans MS',
+      },
+    },
+    appearance: 'dark',
+    disableAppearance: true,
+  }), []);
+
   return (
     <>
       <WidgetEvents />
       <LiFiWidget
-        config={{
-
-chains: {
-  deny: [56,100,250,10,43114,250,25,66,100,122,1284,1285,42220,1313161554,106,288,9001,1666600000,128,137,1],
-},
-          
-         containerStyle: {
-        border: `1px solid ${
-          window.matchMedia('(prefers-color-scheme: dark)').matches
-            ? 'rgb(66, 66, 66)'
-            : 'rgb(234, 234, 234)'
-        }`,
-        borderRadius: '16px',
-      },
-      theme: {
-        palette: {
-          primary: { main: '#9900d1' },
-          secondary: { main: '#F5B5FF' },
-        },
-        shape: {
-          borderRadius: 0,
-          borderRadiusSecondary: 0,
-        },
-        typography: {
-          fontFamily: 'Comic Sans MS',
-        },
-      },
-      appearance: 'dark',
-      disableAppearance: true,
-        }}
+        config={widgetConfig}
         integrator="nextjs-example"
       />
     </>
